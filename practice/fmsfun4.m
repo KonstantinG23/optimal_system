bb6601f62012d3afbd4488b040ea366f230f0b8e
KonstantinG23 <kostya_gr17@mail.ru>
2024-12-18 10:09:13 +0300

    add frquency haracteristics

--- practice/fmsfun4.m
+++ practice/fmsfun4.m
@@ -6,7 +6,7 @@ R = roots(Pol);
 A = [a11 a12 0; a21 a22 0; 1 0 0];
 B = [b11; b21; 0];
 K = acker(A,B,R);
-[t,x] = ode45('odefun',[0 200],[0 0 fi0]);
+[t,x] = ode45('odefun',[0 tm],[0 0 fi0]);
 for i = 1:length(t)
     u(i) = -K(1)*x(i,1)-K(2)*x(i,2)-K(3)*x(i,3);
     if abs(u(i)) >= delta_max
@@ -21,11 +21,26 @@ for i = length(t):-1:1
     end
 end
 f4=tp;
-figure(2)
+
+N = length(t);                 
+Ts = mean(diff(t));            
+Fs = 1/Ts;
+X = fft(x);                    
+f = (0:N-1)*(Fs/N);
+mag = abs(X);                  
+mag_db = 20*log10(mag); 
+
+figure(3)
 subplot(2,1,1),plot(t,x),grid
 xlabel('t')
 legend('\omega_{y}','\beta','\phi')
 subplot(2,1,2),plot(t,uu),grid
 xlabel('t')
 legend('u')
+
+figure(4)
+semilogx(f(1:N/2), mag_db(1:N/2));
+xlabel('frequency, Hz');
+ylabel('Amplitude, dB');
+grid on
 end
--- practice/main.m
+++ practice/main.m
@@ -1,12 +1,22 @@
 clear all
 clc
+close all
 global a11 a12 a21 a22 b11 b21 delta_max tm fi0
-set([figure(1) figure(2)], 'WindowStyle', 'Docked');
-n = 2;
+options = optimset('PlotFcns',@optimplotfval);
+set([figure(1) figure(2) figure(3) figure(4)], 'WindowStyle', 'Docked');
+n = 7; % номер модели судна в соответствии с вариантом в МУ
 config
-w0_baz2 = fminsearch('fmsfun2',[0.1]);
+switch n
+    case 1
+        w = 0.1;
+    case 4
+        w = 0.5;
+    case 7
+        w = 0.5;
+end
+w0_baz2 = fminsearch('fmsfun2',[w]);
 w0_baz2
 tp2 = fmsfun2(w0_baz2)
-w0_baz4 = fminsearch('fmsfun4',[0.1]);
+w0_baz4 = fminsearch('fmsfun4',[w]);
 w0_baz4
 tp4 = fmsfun2(w0_baz4)
